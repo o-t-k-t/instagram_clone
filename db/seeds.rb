f15308bc00ffb7d1b65db447941e14aa9a676a09
helium <wetcondition@gmail.com>
2018-10-17 15:37:00 +0900

    シードデータの日本語対応

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 50.times do
   password = 'password'
-  User.create!(name: Faker::NewGirl.character,
+  User.create!(name: Faker::Name.name,
                email: Faker::Internet.email,
                password: password,
                password_confirmation: password)
